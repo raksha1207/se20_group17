1b798737f315db9d191b6752fceee7bf46fe5e71
raksha1207 <30931862+raksha1207@users.noreply.github.com>
2020-08-15 22:08:20 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='se_grp17',
+    version='0.1',
+    description='Homework 1 Python Tests',
+    author='NCSU CS grp 17',
+    author_email='smavinh@ncsu.edu',
+    
+    packages=['** add packages **'],
+      long_description="""\
+        ADD  DATA 
+      """,
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python",
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "Topic :: Data structures",
+      ],
+      keywords='ADD KEYWORDS',
+      license='MIT',
+      
+      )
